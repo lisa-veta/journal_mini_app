6e31a83bf92630dde8baf6284f2a8490234b4e2d
pe4enka <girfanov_kostia@mail.ru>
2024-11-30 19:08:55 +0500

    added choosing a lesson depending on time

--- src/pages/AttendancePage/AttendancePage.jsx
+++ src/pages/AttendancePage/AttendancePage.jsx
@@ -32,12 +32,39 @@ const AttendancePage = () => {
         { id: 2, date: "02.12.2024", lesson: "2 пара" },
         { id: 3, date: "03.12.2024", lesson: "3 пара" },
         { id: 4, date: "04.12.2024", lesson: "4 пара" },
-        { id: 5, date: "04.12.2024", lesson: "4 пара" },
-        { id: 6, date: "04.12.2024", lesson: "4 пара" },
-        { id: 7, date: "04.12.2024", lesson: "4 пара" },
-        { id: 8, date: "04.12.2024", lesson: "4 пара" },
-        { id: 9, date: "04.12.2024", lesson: "4 пара" },
+        { id: 5, date: "04.12.2024", lesson: "5 пара" },
+        { id: 6, date: "04.12.2024", lesson: "6 пара" },
+        { id: 7, date: "04.12.2024", lesson: "7 пара" },
+        { id: 8, date: "04.12.2024", lesson: "8 пара" }
     ];
+
+    let currentLessonId = 0;
+    
+    const currentMinutes = new Date().getHours() * 60 + new Date().getMinutes();
+    if (currentMinutes >= (7 * 60 + 50) && currentMinutes <= (9 * 60 + 30)) {
+        currentLessonId = 1;
+    }
+    else if (currentMinutes >= (9 * 60 + 30) && currentMinutes <= (11 * 60 + 10)) {
+        currentLessonId = 2;
+    }
+    else if (currentMinutes >= (11 * 60 + 10) && currentMinutes <= (12 * 60 + 50)) {
+        currentLessonId = 3;
+    }
+    else if (currentMinutes >= (12 * 60 + 50) && currentMinutes <= (14 * 60 + 45)) {
+        currentLessonId = 4;
+    }
+    else if (currentMinutes >= (14 * 60 + 45) && currentMinutes <= (16 * 60 + 30)) {
+        currentLessonId = 5;
+    }
+    else if (currentMinutes >= (16 * 60 + 30) && currentMinutes <= (18 * 60 + 10)) {
+        currentLessonId = 6;
+    }
+    else if (currentMinutes >= (18 * 60 + 10) && currentMinutes <= (19 * 60 + 50)) {
+        currentLessonId = 7;
+    }
+    else if (currentMinutes >= (19 * 60 + 50) && currentMinutes <= (21 * 60 + 25)) {
+        currentLessonId = 8;
+    }
     //передаю группу, передаю айди предмета
     return (
         <Layout>
@@ -47,7 +74,7 @@ const AttendancePage = () => {
                     <CustomInfo caption="Преподаватель" content="Лариса Гузеева"/>
                 </div>
                 <div className="attendancePage__table">
-                    <AttendanceTable students={students} schedule={schedule} currentLessonId={2}/>
+                    <AttendanceTable students={students} schedule={schedule} currentLessonId={currentLessonId}/>
                 </div>
             </div>
         </Layout>
